7927ed830ee85f9200cf9cd5698c399587ec9bb3
max <weber.maximilian@gmail.com>
2025-07-02 10:56:57 +0200

    Inline `virtual-thread-factory` creation.

--- routine/src/clj/parts/routine/schedulers.clj
+++ routine/src/clj/parts/routine/schedulers.clj
@@ -6,7 +6,7 @@
    - Light routines are executed immediately on a single virtual thread
      scheduler for short-lived or non-blocking tasks.")
 
-(def ^:private default-heavy-routine-pool-size 2)
+(def default-heavy-routine-pool-size 2)
 
 (defn add-heavy-routine-scheduler
   [w]
@@ -15,13 +15,9 @@
          (java.util.concurrent.Executors/newScheduledThreadPool (::heavy-routine-pool-size w
                                                                   default-heavy-routine-pool-size))))
 
-
-(def ^:private virtual-thread-factory
-  (.factory (Thread/ofVirtual)))
-
 (defn add-light-routine-scheduler
   [w]
   (assoc w
          :routine/light-routine-scheduler
          (java.util.concurrent.Executors/newScheduledThreadPool 1
-                                                                virtual-thread-factory)))
+                                                                (.factory (Thread/ofVirtual)))))
